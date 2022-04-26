0d02b2cbac4f686b6e8ea21824d654c29dfc6f5e
jiyoung park <bewithu14@gmail.com>
2022-04-26 12:32:31 +0900

    자바 2일차 : 비트논리 연산자 예제

---
+++ java/src/day2/Ex8_BitLogic.java
@@ -0,0 +1,18 @@
+package day2;
+
+public class Ex8_BitLogic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*비트 논리 연산자는 양쪽에 값(변수)가 온다. 
+		양쪽에 있는 값을 비트로 나열한 후 논리 연산을 함.
+		1을 참으로 생각하고, 0을 거짓으로 생가하면 됨.
+
+		3: 00000011
+		10: 00001010
+		3&10: 00000010 => 2
+		3|10 00001011 => 11
+		*/
+	}
+
+}
