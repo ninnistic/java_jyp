5793dfbfdd7821b924673a8c91cb0f17c3089a8c
jiyoung park <bewithu14@gmail.com>
2022-04-27 17:38:22 +0900

    자바 3일차 : for문 예제 - Hello 출력 예제

---
+++ java/src/day3/Ex10_For1.java
@@ -0,0 +1,25 @@
+package day3;
+
+public class Ex10_For1 {
+
+	public static void main(String[] args) {
+		/* 콘솔에 Hello를 5번 출력하세요.
+		 * 초기화
+		 * 조건식
+		 * 증감식
+		 * 규칙성
+		 * */
+		
+
+		
+		int i;
+		for(i = 1; i <=5 ; i ++) {
+			System.out.println("Hello");
+		}
+			
+		
+
+	}
+}
+
+
