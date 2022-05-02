96a79d96806f6a27445543659ecb76b906e5d92c
jiyoung park <bewithu14@gmail.com>
2022-05-02 09:22:56 +0900

    자바 5일차 : 중첩 반복문 예제 - 별 예제 - 다른 방법 추가

---
+++ java/src/day5/Ex7_NestedLoop2.java
@@ -0,0 +1,72 @@
+package day5;
+
+public class Ex7_NestedLoop2 {
+
+	public static void main(String[] args) {
+		/*
+		 * 아래와 같이 출력되도록 코드를 작성하세요. 
+		 * * 
+		 * ** 
+		 * *** 
+		 * **** 
+		 * *****
+		 */
+
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		/*
+		 * 아래와 같이 출력되도록 코드를 작성하세요.
+		 *      *	*은 한개, 공백은 4개
+		 *     **
+		 *    ***
+		 *   ****
+		 *  *****
+		 */
+		
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j<=5-i; j++) {
+					System.out.print(" ");
+				} 
+			for (int j = 1; j<=i; j++) {
+					System.out.print("*");
+				}
+			System.out.println();
+		}
+		
+		/* 		 *
+		 * 		***
+		 * 	   *****
+		 * 	  *******
+		 */
+		
+	
+		for (int i=1; i<=5; i++) {
+			for(int j =1; j<=5-i; j++) {
+				System.out.print(" ");
+			}
+			for(int j =1; j<=2*i-1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+	}
+		
+	//방법2
+		
+		for (int i=1; i<=5; i++) {
+			for(int j =1; j<=5-i; j++) {
+				System.out.print(" ");
+			}
+			for(int j =1; j<=2*i-1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		
+
+}
+}
+}
