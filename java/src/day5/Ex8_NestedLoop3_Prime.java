14a75da38106c502649e80fcce8cc5b7949f26e3
jiyoung park <bewithu14@gmail.com>
2022-05-02 09:23:04 +0900

    자바 5일차 : 중첩 반복문 예제 - 소수 출력 예제

---
+++ java/src/day5/Ex8_NestedLoop3_Prime.java
@@ -0,0 +1,25 @@
+package day5;
+
+public class Ex8_NestedLoop3_Prime {
+
+	public static void main(String[] args) {
+		/* 1부터 100사이의 모든 소수를 출력하는 코드를 작성하세요. */
+		// 소수인지를 판별한다.
+
+		for (int num = 1; num <= 100; num++) {
+
+			int count = 0;
+			// i부터 num사이의 num의 약수 개수
+			for (int i = 1; i <= num; i++) {
+				// i가 num의 약수이다 -> num을 i로 나누었을때 나머지가 0과 같다.
+				if (num % i == 0) {
+					count++;
+				}
+			}
+			if (count == 2) {
+				System.out.print(num + " ");
+			}
+		}
+
+	}
+}
