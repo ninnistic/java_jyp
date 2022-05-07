9a3bd7dbeda63c061f104cb7a162ce8d63419f9f
jiyoung park <bewithu14@gmail.com>
2022-05-07 12:00:04 +0900

    자바 10일차 : remoteController

---
+++ java/src/day10/Ex1_Class_RemoteController.java
@@ -0,0 +1,21 @@
+package day10;
+
+public class Ex1_Class_RemoteController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+/*TV 리모컨 클래스를 생성하고, 객체를 생성 후 기능들을 테스트해보세요.*/
+
+class RemoteController{
+	/* 기능들 
+	 * on/off
+	 * 채널 switching
+	 * 
+	 */
+boolean on; //off 기능
+}
