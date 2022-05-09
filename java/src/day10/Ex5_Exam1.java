ba78a50c744a19f9c15234daca830e902e407a72
ninnistic <bewithu14@gmail.com>
2022-05-09 17:04:05 +0900

    시험1_최종

--- java/src/day10/Ex5_Exam1.java
+++ java/src/day10/Ex5_Exam1.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class Ex5_Exam1 {
 
 	public static void main(String[] args) {
+
 		Scanner scan = new Scanner(System.in);
 		int menu = -1;
 		Student stu = new Student(null, 0, 0, 0);
 
 		while (true) {
-			System.out.println("메뉴\n 1.학생정보 입력\n 2. 학생정보 출력 \n 3. 프로그램 종료");
+			System.out.println("메뉴\n 1. 학생정보 입력\n 2. 학생정보 출력 \n 3. 프로그램 종료");
 			System.out.println("메뉴를 입력하세요 : ");
 			menu = scan.nextInt();
 			switch (menu) {
 			case 1:
-				// 생성자를 호출해서 넣어야 하는건가?
 				System.out.println("학생정보 입력하세요. :   ");
-				String name = scan.nextLine();
+				String name = scan.next();
 				int grade = scan.nextInt();
 				int classNum = scan.nextInt();
 				int number = scan.nextInt();
@@ -28,14 +28,14 @@ public class Ex5_Exam1 {
 				break;
 			case 3:
 				System.out.println("프로그램을 종료합니다.");
-				break;
+				return;
 			default:
 				System.out.println("잘못된 메뉴입니다.");
 			}
-			scan.close();
 		}
 
 	}
+
 }
 
 class Student {
@@ -52,6 +52,7 @@ class Student {
 	}
 
 	void print() {
-		System.out.println("이름 : " + name + "," + "학년 : " + grade + "," + "반 : " + classNum + "," + "번호 : " + number);
+		System.out.println(
+				"이름 : " + name + " , " + "학년 : " + grade + " , " + "반 : " + classNum + " , " + "번호 : " + number);
 	}
 }
