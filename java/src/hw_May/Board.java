0d846910e1e8af72904625610749c84dde76f3e2
ninnistic <bewithu14@gmail.com>
2022-05-15 18:18:10 +0900

    게시판 만들기 - 2nd Commit (기획)

--- java/src/hw_May/Board.java
+++ java/src/hw_May/Board.java
@@ -1,10 +1,81 @@
 package hw_May;
 
+// 오 대박 클래스를 두개로 나누는 이유는 Board에서는 이제, Board내부에서 진행되는 메소드들을 넣는것..?
+// + 아닌듯... Board는 말 그대로 게시판에서 사용되는 기능들의 선언..? 정도로 가져가야 할 것 같다. 
+// ++ 서버에서 게시판이 build되는 과정으로 봐야할까???
+// +++ 서버 내부에서는 그러면... 게시판의 속성 / 게시판 생성자 / 게시판 관리자가 게시판 항목이랑... 게시판 항목 삭제가 가능하게 하는 method? 
+// ++++ 쉽게 말하면 관리자의 영역 
+
+//필요한 field : 아이디, 닉네임, 공지(공지 tag), 제목, 글 내용, 조회수, 날짜, 
 public class Board {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private String id; 
+	private String username;
+	private String notice;
+	private String title;
+	private String contents;
+	private int view;
+	
+	public Board(String id, String username, String notice, String title, String contents, int view) {
+		this.id = id;
+		this.username = username;
+		this.notice = notice;
+		this.title = title;
+		this.contents = contents;
+		this.view = view;
+	}
+	
+	// Getters and Setters 
+	public String getId() {
+		return id;
 	}
 
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getNotice() {
+		return notice;
+	}
+
+	public void setNotice(String notice) {
+		this.notice = notice;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public int getView() {
+		return view;
+	}
+
+	public void setViewCount(int viewCount) {
+		this.view = viewCount;
+	}
+	
+	 // 1. 무슨 기능이 있는지 print해주는 메소드. (읽는)
+	// 2. 필요한 항목을 수정할 수 있는 메소드.   /// Question. - 수정과 추가는 같은 영역일까 다른 영역일까??? 다른듯 추가는 늘어나는거니까?
+	// 3. 항목을 삭제할 수 있는 메소드. 
+	// 4. 항목을 추가 할 수 있는 메소드. // 여기까지는 서버의 영역이라고 생각하면 class를 나누는게 이해가 그나마 수월해질 것 같다...
 }
+
--- java/src/hw_May/Community.java
+++ java/src/hw_May/Community.java
@@ -3,77 +3,21 @@ package hw_May;
 import java.util.Scanner;
 
 import day14.Item;
+// Community에서는 외부에서 진행되는 기능들을 넣으면 되겠다!!
+// 그러니까 Board 바깥에서, Board를 이용하는 사람들이 실제 행하는 method를 넣는다고 생각해야겠다.
+// 실제 user가 동작하기 위해 필요한 기능...or 이용하기 위해 필요한 function을 넣는다고 생각하면 될 것 같다.
+// 쉽게 말하면 user가 쓰는 영역??? 으로 보면 이해가 쉬울 것 같다. 흠 
+// community 상위에 Console을 만들어서 implemented를 해서 실행하는게 간결 할 것 같다...아마도
 
 public class Community {
-	//필요한 field : 아이디, 닉네임, 공지(공지 tag), 제목, 글 내용, 조회수, 날짜, 
-	private String id; 
-	private String username;
-	private String notice;
-	private String title;
-	private String contents;
-	private int view;
 	public static final int MAX = 100;
 	private Board board[] = new Board[MAX];
 	Scanner scan = new Scanner(System.in);
 	
-	// Constructor
-	public Community(String id, String username, String notice, String title, String contents, int view) {
-		this.id = id;
-		this.username = username;
-		this.notice = notice;
-		this.title = title;
-		this.contents = contents;
-		this.view = view;
-	}
+	//실제 web을 만약 만든다면 서버에서 불러와야 하는 정보들???
 	
-	// Getters and Setters 
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getNotice() {
-		return notice;
-	}
-
-	public void setNotice(String notice) {
-		this.notice = notice;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getContents() {
-		return contents;
-	}
-
-	public void setContents(String contents) {
-		this.contents = contents;
-	}
-
-	public int getView() {
-		return view;
-	}
-
-	public void setViewCount(int viewCount) {
-		this.view = viewCount;
-	}
+	// Constructor
+	
 
 	
 	// 게시글이 몇개 적혀 있나 확인하는 기능 
@@ -107,6 +51,32 @@ public class Community {
 		}
 	}
 
+	//1. 글쓰기 항목
+	/// 일반쓰기와 공지쓰기로 나눔 / 
+	// 일반쓰기는 순서에 따라서 1,2,3,4 넘버링 되면서 글이 계속 추가가 된다.
+	// 하지만 공지쓰기는 글을 checking할때마다 매번 최상단에 존재해야 한다. 그러면 어떻게 해야할까..?
+	
+	// 1) 글쓰기를 하면 board의 배열안에 저장이 된다. 번지수대로..
+	// 2) 공지는...쓸수 있는 수를 제한을 둬야겠다. 최대 3개 (무한으로 공지를 쓰는 경우는 생각하기 어렵다. )
+	// 3) 공지 1개를 작성하면, 가장 최신 글과 번지수를 swap해서 0번지,1번지,2번지에 오게 해야한다.
+	// 4) 만약 공지가 1개 작성된 상태에서 글을 작한다면/???? 어렵다ㅏㅏㅏ...
+	// 공지게시판과 자유게시판으로 나누는 선택도 할 수 있다. -----
+	// 5) 글 쓸 때 nickname, id를 추가하고 날짜를...자동으로 나오게 하려면 LocalDateTime 이용?
+	// 5-2) LocalDateTime 이 안되면 -> 그냥 날짜도 적게 만들어야한다.
+	
+	//2. 게시글 확인하기
+	//Board에서 출력 method 이용.
+	// 확인할때마다 count++; print랑 count를 엮어서 조회수를 늘려야 한다. 그럼 조회수를 위한 method도.. 만들어야겠다.
+	// Board에 선언된 view를 ++; 하기 위해서 게시글 확인할때 view를 매개변수로 받아와야 하는건가?
+	
+	
+	//3. 게시글 수정하기
+	//AccountBook에서 했던것처럼 수정 method 이용.
+	// 
+	
+	//4. 종료하기 
+	
+	
 }
 
 
