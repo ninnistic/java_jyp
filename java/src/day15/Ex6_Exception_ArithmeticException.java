27c9e19ac6641f93f666a8f52e214da528d8b25c
ninnistic <bewithu14@gmail.com>
2022-05-17 17:08:43 +0900

    자바 15일차 - 예외 예제 - ArithmeticException 예제

---
+++ java/src/day15/Ex6_Exception_ArithmeticException.java
@@ -0,0 +1,14 @@
+package day15;
+
+public class Ex6_Exception_ArithmeticException {
+
+	public static void main(String[] args) {
+		// 산술 연산자를 이용하다가 발생하는 예외
+		// 나누기나 나머지 구하기 (/ , %) 를 이용할때 발생할 수 있다.
+		
+		int num1 = 1, num2 = 0;
+		System.out.println(num1 / num2);
+
+	}
+
+}
