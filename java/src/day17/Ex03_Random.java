b5e98a3e7549102def42b6fb384c47178f3ebc5d
ninnistic <bewithu14@gmail.com>
2022-05-18 23:25:50 +0900

    자바 17일차 :  Random prac

---
+++ java/src/day17/Ex03_Random.java
@@ -0,0 +1,37 @@
+package day17;
+
+import java.util.Random;
+
+public class Ex03_Random {
+
+	public static void main(String[] args) {
+		/* Random 생성자에 시드값Random(); 을 입력 안하면, ㅎ녀재시간을 시드값으로랜덤한 수를 생성
+		 * Random 생성자에 시드값을 입력하면 입력한 값을 이용하여 랜덤한 수를 생성하기 때문에
+		 * 항상 같은 순서로 랜덤한 값이 고정된다.
+		 */
+		Random r = new Random();
+		int num = 5;
+		for (int i = 0; i<num; i ++) {
+			//0이상 1미만의 난수
+			System.out.println(r.nextDouble());
+		}
+		System.out.println("===================");
+		int min = 1, max = 9;
+		for (int i = 0; i<num; i ++) {
+			//int의 최소값에서 최대값 사이의 난수가 나온다.
+			System.out.println(r.nextInt(max - min + 1) + min);
+			
+	}
+		System.out.println("===================");
+		for (int i = 0; i<num; i ++) {
+			//int의 최소값에서 최대값 사이의 난수가 나온다.
+			System.out.println(r.nextInt());
+		}
+		
+		/* Matho.random()은 객체를 생성 안하고 호출할 수 있짐나 식이()가 많아서 복잡하다.
+		 * Random클래스의 nextInt(num)은 객체를 생성하고 호출해야 하지만 식이 간단하다.
+		 * 취향에 맞게 선택한다.
+		 */
+	}
+
+}
