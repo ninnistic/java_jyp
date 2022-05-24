1c709f38c247fbde90758d646ff0831660840602
ninnistic <bewithu14@gmail.com>
2022-05-24 09:38:23 +0900

    자바 20일차 : 시험 (Java)

---
+++ java/src/day20/Test3.java
@@ -0,0 +1,62 @@
+package day20;
+
+import java.util.Scanner;
+import java.util.*;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+
+		Scanner scan = new Scanner(System.in);
+		int exitMenu = 4;
+		int menu;
+		String fileName;
+
+		do {
+			printMenu();
+			menu = scan.nextInt();
+			switch (menu) {
+			case 1:
+				System.out.print("파일명 : ");
+				fileName = scan.next();
+				list.add(fileName);
+				System.out.println("파일이 저장 되었습니다.");
+				break;
+			case 2:
+				for (int i = 0; i < list.size(); i++) {
+					System.out.println(list.get(i) + " ");
+				}
+				System.out.println();
+
+				break;
+			case 3:
+				System.out.print("검색어 : ");
+				String keyword = scan.next();
+				for (String tmp : list) {
+					if (tmp.contains(keyword) == true) {
+						System.out.println(tmp);
+
+					}
+				}
+
+				break;
+			case 4:
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			default:
+				System.out.println("잘못 입력 되었습니다.");
+			}
+		} while (menu != exitMenu);
+	}
+
+	public static void printMenu() {
+		System.out.println("메뉴");
+		System.out.println("1. 파일 저장");
+		System.out.println("2. 파일 확인");
+		System.out.println("3. 파일 검색");
+		System.out.println("4. 프로그램 종료");
+		System.out.print("메뉴를 선택하세요 : ");
+	}
+
+}
