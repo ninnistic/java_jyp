c2e5996e3933ea25feba9662bb1ef33384ac85b9
ninnistic <bewithu14@gmail.com>
2022-05-24 09:34:45 +0900

    자바 18일차 : Stack 설명

---
+++ java/src/day18/Ex02_Stack.java
@@ -0,0 +1,15 @@
+package day18;
+
+public class Ex02_Stack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		test();
+	}
+	
+	/*재귀 메소드는 메소드 안에서 자기 자신을 호출하는 메소드 */
+	public static void test() {
+		test();
+	}
+
+}
